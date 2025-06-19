101ab5a55b39314e90779e991dbd809a322ce580
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-19 15:40:58 +0500

    Update InputField.jsx

--- my-form/src/components/InputField.jsx
+++ my-form/src/components/InputField.jsx
@@ -8,7 +8,7 @@ function InputField({ label, name, type, value, onChange }) {
         value={value}
         onChange={onChange}
         required
-        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="w-full px-4 py-2 border rounded-lg  "
       />
     </div>
   );
