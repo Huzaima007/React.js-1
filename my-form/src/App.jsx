7a3872a98836fe2a2bb968dbb88c5f5dc5a9abc8
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-20 18:49:06 +0500

    Update App.jsx

--- my-form/src/App.jsx
+++ my-form/src/App.jsx
@@ -1,9 +1,17 @@
-
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
 import Form from "./components/Form";
+import Detail from "./components/Detail";
+import Basic from "./components/Formik";
 
 function App() {
-  return <Form />;
+  return (
+    <Routes>
+      <Route index element={<Form />} />
+      <Route path="detail" element={<Detail />} />
+      <Route path="basic" element={<Basic />} />
+    </Routes>
+  );
 }
 
 export default App;
