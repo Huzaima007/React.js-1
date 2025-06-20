4536249f316a295b117f4d7adb9652b8157c71fd
Hanzala <160530387+Huzaima007@users.noreply.github.com>
2025-06-20 18:49:03 +0500

    Update Form.jsx

--- my-form/src/components/Form.jsx
+++ my-form/src/components/Form.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import InputField from "./InputField";
+import { useNavigate } from "react-router";
 
 function Form() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -16,52 +18,66 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form Submitted:", formData);
+
+    navigate("detail", { state: { data: formData, fromForm: true } });
   };
 
   return (
-   <div className="flex items-center justify-center min-h-screen bg-sky-400 px-4">
-  <form
-    onSubmit={handleSubmit}
-    className="w-full max-w-md bg-gray-50 p-6 sm:p-8 rounded-lg shadow-lg"
-  >
-    <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-gray-800">
-      Email-Form
-    </h2>
+    <div className="flex items-center justify-center min-h-screen bg-sky-400 px-4">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-gray-50 p-6 sm:p-8 rounded-lg shadow-lg"
+      >
+        <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-gray-800">
+          Email-Form
+        </h2>
 
-    <InputField
-      label="Email"
-      type="email"
-      name="email"
-      value={formData.email}
-      onChange={handleChange}
-    />
+        <InputField
+          label="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
 
-    <InputField
-      label="Password"
-      type="password"
-      name="password"
-      value={formData.password}
-      onChange={handleChange}
-    />
+        <InputField
+          label="Password"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
 
-    <InputField
-      label="Confirm Password"
-      type="password"
-      name="confirmPassword"
-      value={formData.confirmPassword}
-      onChange={handleChange}
-    />
+        <InputField
+          label="Confirm Password"
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
 
-    <button
-      type="submit"
-      className="mt-4 w-full bg-gray-500 text-white py-2 rounded-lg hover:bg-gray-600 transition duration-300"
-    >
-      Submit
-    </button>
-  </form>
-</div>
-
+        <button
+          type="submit"
+          className="mt-4 w-full bg-gray-500 text-white py-2 rounded-lg hover:bg-gray-600 transition duration-300"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
 export default Form;
+
+
+
+
+
+
+
+
+
+
+
+
+
